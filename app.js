eeb275d90817c53720a5a33cc32054b6b3dd5eca
Tatyana Tupalo <tatyana080489t@gmail.com>
2023-06-28 22:42:00 +0300

    add server to app js

--- app.js
+++ app.js
@@ -31,4 +31,25 @@ app.use((req, res) => {
   res.status(status).json({ message });
 });
 
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = require("../app");
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
 module.exports = app;

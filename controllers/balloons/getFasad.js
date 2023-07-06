e5aadf61997de4500fcb87af1316fe0cc8dcf83f
Tatyana Tupalo <tatyana080489t@gmail.com>
2023-07-06 15:41:10 +0300

    fix fasad

--- controllers/balloons/getFasad.js
+++ controllers/balloons/getFasad.js
@@ -1,7 +1,7 @@
 const { Balloon } = require("../../models");
 
 const getFasad = async (req, res) => {
-  const result = await Balloon.find({ category: "Фасади" });
+  const result = await Balloon.find({ category: "Оформлення фасадів" });
   res.json({
     status: "success",
     code: 200,
